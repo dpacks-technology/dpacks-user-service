e6e16510142991f1bb835c5d0e3286c017d0bdd8
Yashodha Fernando <118588673+Yashoda-Fr@users.noreply.github.com>
2024-03-19 19:14:32 +0530

    feat:dashboar route

--- controllers/analytics_alerts_controller.go
+++ controllers/analytics_alerts_controller.go
@@ -8,53 +8,6 @@ import (
 	"net/http"
 )
 
-// // GetAnalyticalAlerts function
-//
-//	func GetAnalyticalAlerts(db *sql.DB) gin.HandlerFunc {
-//		return func(c *gin.Context) {
-//
-//			// Query the database for all records
-//			rows, err := db.Query("SELECT * FROM useralerts")
-//
-//			if err != nil {
-//				fmt.Printf("%s\n", err)
-//				c.JSON(http.StatusInternalServerError, gin.H{"error": "Error querying the database"})
-//				return
-//			}
-//
-//			//close the rows when the surrounding function returns(handler function)
-//			defer rows.Close()
-//
-//			// Iterate over the rows and scan them into UserAlerts structs
-//			var userAlerts []models.UserAlerts
-//
-//			for rows.Next() {
-//				var userAlert models.UserAlerts
-//				if err := rows.Scan(&userAlert.AlertID, &userAlert.UserID, &userAlert.UserEmail, &userAlert.AlertThreshold, &userAlert.AlertSubject, &userAlert.AlertContent, &userAlert.WhenAlertRequired, &userAlert.ReminderOption, &userAlert.CustomReminderDate); err != nil {
-//					fmt.Printf("%s\n", err)
-//					c.JSON(http.StatusInternalServerError, gin.H{"error": "Error scanning rows from the database"})
-//					return
-//				}
-//				userAlerts = append(userAlerts, userAlert)
-//			}
-//
-//			//this runs only when loop didn't work
-//			if err := rows.Err(); err != nil {
-//				fmt.Printf("%s\n", err)
-//				c.JSON(http.StatusInternalServerError, gin.H{"error": "Error iterating over rows from the database"})
-//				return
-//			}
-//
-//			// Return all userAlerts as JSON
-//			c.JSON(http.StatusOK, userAlerts)
-//
-//		}
-//	}
-//
-// delete alert
-
-// update alert
-
 // create  new alert
 func CreateAlert(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -79,6 +32,7 @@ func CreateAlert(db *sql.DB) gin.HandlerFunc {
 	}
 }
 
+// delete alert
 func DeleteAlert(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 
@@ -109,6 +63,7 @@ func DeleteAlert(db *sql.DB) gin.HandlerFunc {
 	}
 }
 
+// update alert
 func UpdateAlert(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		id := c.Param("id")
@@ -174,5 +129,9 @@ func GetAlertList(db *sql.DB) gin.HandlerFunc {
 }
 
 func GetDashboardData(db *sql.DB) gin.HandlerFunc {
-	
+	return func(c *gin.Context) {
+		rows, err := db.Query("SELECT  FROM useralerts WHERE userid = $1", id)
+
+	}
+
 }

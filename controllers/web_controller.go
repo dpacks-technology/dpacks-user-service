08e60a0b157c4a2d08899751437e0c8a07e2fbcc
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-03-20 10:57:32 +0530

    feat: webpages status read

--- controllers/web_controller.go
+++ controllers/web_controller.go
@@ -36,13 +36,13 @@ func GetWebPages(db *sql.DB) gin.HandlerFunc {
 			return
 		}
 
+		// Calculate offset
+		offset := (pageInt - 1) * countInt
+
 		// get query parameters
 		key := c.Query("key")
 		val := c.Query("val")
 		escapedVal := "%" + strings.ReplaceAll(val, "_", "\\_") + "%"
-
-		// Calculate offset
-		offset := (pageInt - 1) * countInt
 
 		var args []interface{}
 
@@ -56,10 +56,10 @@ func GetWebPages(db *sql.DB) gin.HandlerFunc {
 				query = "SELECT * FROM webpages WHERE id = $3 ORDER BY id LIMIT $1 OFFSET $2"
 				args = append(args, val)
 			case "name":
-				query = "SELECT * FROM webpages WHERE name LIKE $3 ORDER BY id LIMIT $1 OFFSET $2"
+				query = "SELECT * FROM webpages WHERE name LIKE $3 ORDER BY CASE WHEN name = $3 THEN 1 ELSE 2 END, id LIMIT $1 OFFSET $2"
 				args = append(args, escapedVal)
 			case "path":
-				query = "SELECT * FROM webpages WHERE path LIKE $3 ORDER BY id LIMIT $1 OFFSET $2"
+				query = "SELECT * FROM webpages WHERE path LIKE $3 ORDER BY CASE WHEN path = $3 THEN 1 ELSE 2 END, id LIMIT $1 OFFSET $2"
 				args = append(args, escapedVal)
 			}
 		}
@@ -71,6 +71,184 @@ func GetWebPages(db *sql.DB) gin.HandlerFunc {
 			return
 		}
 		defer stmt.Close()
+
+		// Execute the prepared statement with bound parameters
+		rows, err := stmt.Query(args...)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+
+		//close the rows when the surrounding function returns(handler function)
+		defer rows.Close()
+
+		// Iterate over the rows and scan them into WebpageModel structs
+		var webpages []models.WebpageModel
+
+		for rows.Next() {
+			var webpage models.WebpageModel
+			if err := rows.Scan(&webpage.ID, &webpage.Name, &webpage.WebID, &webpage.Path, &webpage.Status, &webpage.DateCreated); err != nil {
+				fmt.Printf("%s\n", err)
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Error scanning rows from the database"})
+				return
+			}
+			webpages = append(webpages, webpage)
+		}
+
+		//this runs only when loop didn't work
+		if err := rows.Err(); err != nil {
+			fmt.Printf("%s\n", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error iterating over rows from the database"})
+			return
+		}
+
+		// Return all webpages as JSON
+		c.JSON(http.StatusOK, webpages)
+
+	}
+}
+
+// GetWebPagesByStatusCount handles GET /api/web/webpages/status/:status/count - READ
+func GetWebPagesByStatusCount(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		// get status parameter (array)
+		statuses := c.Query("status")
+
+		// get query parameters
+		key := c.Query("key")
+		val := c.Query("val")
+
+		var args []interface{}
+		var query string
+
+		query = "SELECT COUNT(*) FROM webpages"
+
+		switch statuses {
+		case "1":
+			query = "SELECT COUNT(*) FROM webpages WHERE status IN ($1)"
+			args = append(args, 1)
+		case "0":
+			query = "SELECT COUNT(*) FROM webpages WHERE status IN ($1)"
+			args = append(args, 0)
+		}
+
+		if val != "" && key != "" {
+
+			escapedVal := "%" + strings.ReplaceAll(val, "_", "\\_") + "%"
+
+			switch key {
+			case "id":
+				query = "SELECT COUNT(*) FROM webpages WHERE id = $2 AND status IN ($1)"
+				args = append(args, val)
+			case "name":
+				query = "SELECT COUNT(*) FROM webpages WHERE name LIKE $2 AND status IN ($1)"
+				args = append(args, escapedVal)
+			case "path":
+				query = "SELECT COUNT(*) FROM webpages WHERE path LIKE $2 AND status IN ($1)"
+				args = append(args, escapedVal)
+			}
+		}
+
+		// Prepare the statement
+		stmt, err := db.Prepare(query)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+
+		// Execute the prepared statement with bound parameters
+		var count int
+		err = stmt.QueryRow(args...).Scan(&count)
+
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+
+		// Close the statement
+		defer stmt.Close()
+
+		// Return all webpages as JSON
+		c.JSON(http.StatusOK, count)
+
+	}
+}
+
+// GetWebPagesByStatus handles GET /api/web/webpages/status/:status - READ
+func GetWebPagesByStatus(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		// get status parameter (array)
+		statuses := c.Query("status")
+
+		// get page id parameter
+		page := c.Param("page")
+
+		// get count parameter
+		count := c.Param("count")
+
+		// Convert page and count to integers
+		pageInt, err := strconv.Atoi(page)
+		if err != nil {
+			// Handle error
+			fmt.Printf("%s\n", err)
+			return
+		}
+
+		countInt, err := strconv.Atoi(count)
+		if err != nil {
+			// Handle error
+			fmt.Printf("%s\n", err)
+			return
+		}
+
+		// Calculate offset
+		offset := (pageInt - 1) * countInt
+
+		// get query parameters
+		key := c.Query("key")
+		val := c.Query("val")
+
+		var args []interface{}
+		var query string
+
+		query = "SELECT * FROM webpages ORDER BY id LIMIT $1 OFFSET $2"
+		args = append(args, countInt, offset)
+
+		switch statuses {
+		case "1":
+			query = "SELECT * FROM webpages WHERE status IN ($3) ORDER BY id LIMIT $1 OFFSET $2"
+			args = append(args, 1)
+		case "0":
+			query = "SELECT * FROM webpages WHERE status IN ($3) ORDER BY id LIMIT $1 OFFSET $2"
+			args = append(args, 0)
+		}
+
+		if val != "" && key != "" {
+
+			escapedVal := "%" + strings.ReplaceAll(val, "_", "\\_") + "%"
+
+			switch key {
+			case "id":
+				query = "SELECT * FROM webpages WHERE status IN ($3) ORDER BY id LIMIT $1 OFFSET $2"
+				query = "SELECT * FROM webpages WHERE id = $4 AND status IN ($3) ORDER BY id LIMIT $1 OFFSET $2"
+				args = append(args, val)
+			case "name":
+				query = "SELECT * FROM webpages WHERE name LIKE $4 AND status IN ($3) ORDER BY id LIMIT $1 OFFSET $2"
+				args = append(args, escapedVal)
+			case "path":
+				query = "SELECT * FROM webpages WHERE path LIKE $4 AND status IN ($3) ORDER BY id LIMIT $1 OFFSET $2"
+				args = append(args, escapedVal)
+			}
+		}
+
+		// Prepare the statement
+		stmt, err := db.Prepare(query)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
 
 		// Execute the prepared statement with bound parameters
 		rows, err := stmt.Query(args...)

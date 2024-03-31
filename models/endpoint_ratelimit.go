41216151762fb974b6f952903d0b14c5e8bd0962
Gayashaan Krishnamoorthy <gayashaan49@gmail.com>
2024-03-31 19:35:59 +0530

    fix:add createdOn column to endpointRateLimit Model

--- models/endpoint_ratelimit.go
+++ models/endpoint_ratelimit.go
@@ -1,7 +1,8 @@
 package models
 
 type EndpointRateLimit struct {
-	Id    int    `json:"id"`
-	Path  string `json:"path"`
-	Limit int    `json:"limit"`
+	Id        int    `json:"id"`
+	Path      string `json:"path"`
+	Limit     int    `json:"limit"`
+	CreatedOn string `json:"created_on"`
 }

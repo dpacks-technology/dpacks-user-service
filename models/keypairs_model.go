4593a49f26f36c4f6c6cfb20d76f9a892089593c
Gayashaan Krishnamoorthy <gayashaan49@gmail.com>
2024-03-31 12:27:40 +0530

    fix:update the key pair model

--- models/keypairs_model.go
+++ models/keypairs_model.go
@@ -2,9 +2,9 @@ package models
 
 // KeyPairs struct is a row record of the keyPairs table in the postgres database
 type KeyPairs struct {
-	ID         int    `json:"id"`
-	UserID     string `json:"user_id"`
-	ClientID   string `json:"client_id"`
-	Key        string `json:"key"`
-	Created_on string `json:"created_on"`
+	ID        int    `json:"id"`
+	UserID    string `json:"user_id"`
+	ClientID  string `json:"client_id"`
+	Key       string `json:"key"`
+	CreatedOn string `json:"created_on"`
 }

a52a07e70bb9d0b77944ad0541be833205c5aa34
Gayashaan Krishnamoorthy <gayashaan49@gmail.com>
2024-03-31 14:58:26 +0530

    fix: error msg

--- validators/apisubscriber_validator.go
+++ validators/apisubscriber_validator.go
@@ -8,7 +8,7 @@ import (
 func ValidateUserId(kepair models.KeyPairs, create bool) error {
 
 	if kepair.UserID == "" {
-		return errors.New("name cannot be empty")
+		return errors.New("UserId cannot be empty")
 	}
 
 	return nil

cfca2a2c3f9cc03c55ac8d8dbaf9408cdab46e6a
Yashodha Fernando <118588673+Yashoda-Fr@users.noreply.github.com>
2024-04-01 18:16:20 +0530

    feat : model created for alerts

--- models/analytical_alerts_model.go
+++ models/analytical_alerts_model.go
@@ -12,3 +12,15 @@ type UserAlerts struct {
 	ReminderOption     string `json:"reminder_option"`
 	CustomReminderDate string `json:"custom_reminder_date"`
 }
+type UserAlertsModel struct {
+	AlertID            int    `json:"alert_id"`
+	UserID             int    `json:"user_id"`
+	UserEmail          string `json:"user_email"`
+	AlertThreshold     int    `json:"alert_threshold"`
+	AlertSubject       string `json:"alert_subject"`
+	AlertContent       string `json:"alert_content"`
+	WhenAlertRequired  string `json:"when_alert_required"`
+	RepeatOn           string `json:"repeat_on"`
+	CustomReminderDate string `json:"custom_reminder_date"`
+	Status             int    `json:"status"`
+}

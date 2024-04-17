a868471f38740132eb81ac1a5e5a5af1aa61c669
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-04-17 09:21:16 +0530

    feat: auth middleware

--- routes/routes.go
+++ routes/routes.go
@@ -31,11 +31,11 @@ func SetupRoutesFunc(r *gin.Engine, db *sql.DB) {
 
 		webRoutes := api.Group("/web") // web api group
 		{
-			webRoutes.POST("/site", controllers.AddSite(db))          // add site
-			webRoutes.GET("/sites", controllers.ReadSites(db))        // read all sites
-			webRoutes.GET("/site/:id", controllers.GetSiteById(db))   // read site by id
-			webRoutes.PUT("/site/:id", controllers.EditSite(db))      // edit site by id
-			webRoutes.DELETE("/site/:id", controllers.DeleteSite(db)) // delete site by id
+			webRoutes.POST("/site", controllers.AddSite(db))                                    // add site
+			webRoutes.GET("/sites", middleware.UserAuthMiddleware(), controllers.ReadSites(db)) // read all sites
+			webRoutes.GET("/site/:id", controllers.GetSiteById(db))                             // read site by id
+			webRoutes.PUT("/site/:id", controllers.EditSite(db))                                // edit site by id
+			webRoutes.DELETE("/site/:id", controllers.DeleteSite(db))                           // delete site by id
 
 			webRoutes.POST("/webpage", controllers.AddWebPage(db)) // add webpage
 

8cff86bfee9d88054745956d9b85f8e163a9c6ae
Gayashaan Krishnamoorthy <gayashaan49@gmail.com>
2024-04-18 20:11:57 +0530

    fix: change phone type to string

--- models/admin_user_model.go
+++ models/admin_user_model.go
@@ -3,7 +3,7 @@ package models
 type AdminUserModel struct {
 	ID       int    `json:"id"`
 	Name     string `json:"name"`
-	Phone    int    `json:"phone"`
+	Phone    string `json:"phone"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 	AddedOn  string `json:"added_on"`
